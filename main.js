0fc8ee53621e399533a73b31e76f0a88996162b1
Wil-Quispe <newwilcode@gmail.com>
2022-06-29 17:11:59 -0500

    Valid Parentheses

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log('hola mundo')
+console.clear()
+
+const isValid = s => {
+  const stack = []
+  const valid = { '(': ')', '[': ']', '{': '}' }
+
+  if (s.length <= 0) return false
+
+  for (const bracket of s) {
+    if (stack.length > 0 && bracket === stack[stack.length - 1]) {
+      stack.pop()
+    } else if (valid[bracket]) {
+      stack.push(valid[bracket])
+    } else {
+      return false
+    }
+  }
+  console.log({ length: stack.length, stack })
+
+  return stack.length === 0
+}
+// console.log(isValid('(){[[]}'))
+console.log(isValid('(}())){{}}'))
+// console.log(isValid('([]){}'))
